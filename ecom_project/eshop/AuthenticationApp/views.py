dac8ac690ebbd6bd9a8da547d71449cd783f230e
Abderazzake <abderazake152@gmail.com>
2024-01-03 23:17:56 +0100

    regist
    Creation de compte ajouté

--- ecom_project/eshop/AuthenticationApp/views.py
+++ ecom_project/eshop/AuthenticationApp/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User , auth
+from django.contrib import messages
 
 # Create your views here.
 
 def login(request):
     return render(request,'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+        email = request.POST['email']
+
+        if password == password1:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'user khdawo')
+                return redirect('login')
+
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'email deja khdawo')
+                return redirect('login')
+
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                print('User created')
+                return redirect('login')
+        else:
+            print('Passwords do not match')
+            return redirect('/')
+
+
+            
+
+    return render(request, "logi.html")
--- ecom_project/eshop/eshop/urls.py
+++ ecom_project/eshop/eshop/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('MainApp.urls')),
-    path('',include('CartApp.urls')),
-    path('',include('AuthenticationApp.urls')),
-    path('',include('PaymentApp.urls')),
-    path('',include('ProductsApp.urls')),
+    path('', include('MainApp.urls')), 
+    path('', include('CartApp.urls')),  
+    path('AuthenticationApp/', include('AuthenticationApp.urls')),  
+    path('', include('PaymentApp.urls')),  
+    path('', include('ProductsApp.urls')),  
 ]
 
+urlpatterns= urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
