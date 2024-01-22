485d6016dd78e0d26bdf78fd746ff0e0e8c78376
Abderazzake <abderazake152@gmail.com>
2024-01-22 11:57:43 +0100

    ch

--- ecom_project/eshop/ProductsApp/models.py
+++ ecom_project/eshop/ProductsApp/models.py
@@ -24,16 +24,16 @@ class Variation(models.Model):
 
     from django.db import models
 
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
-    description = models.TextField(null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    qty = models.IntegerField()  # Make sure this line is present
-    image = models.ImageField(upload_to='products/')
+# class Product(models.Model):
+#     name = models.CharField(max_length=100)
+#     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+#     description = models.TextField(null=True)
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     qty = models.IntegerField()  # Make sure this line is present
+#     image = models.ImageField(upload_to='products/')
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
     
 
 class VariationOption(models.Model):
